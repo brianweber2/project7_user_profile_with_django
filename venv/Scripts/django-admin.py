2cbcf60007ab883805b978c29fd405f08c98f78a
Brian Weber <brianweber2@gmail.com>
2017-01-11 21:19:37 -0800

    Initial commit

---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\Brian\Dropbox\Python\project7_user_profile_with_django\venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
